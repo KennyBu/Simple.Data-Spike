2abdd24dcfd2ba2282559856451f7fcda8a4211e
Ken Burkhardt <ken.burkhardt@gmail.com>
2011-12-16 10:44:52 -0500

    added bulk inserts with transactions

--- SimpleData.Console/Program.cs
+++ SimpleData.Console/Program.cs
@@ -22,7 +22,8 @@ namespace SimpleData.Console
 
             db.Users.DeleteAll();
 
-            PrepareUsers();
+            //PrepareUsers(1000,true);
+            PrepareUsersWithTransaction(10,true,false);
 
             FindOneUserWithoutPoco();
 
@@ -37,37 +38,93 @@ namespace SimpleData.Console
             System.Console.ReadLine();
         }
 
-        private static void PrepareUsers()
+        private static void PrepareUsers(int userCreationCount, bool isBulkInsert)
         {
-            var usersList = Builder<User>.CreateListOfSize(5)
+            var usersList = Builder<User>.CreateListOfSize(userCreationCount)
                 .All()
                     .With(x=>x.Id = Guid.NewGuid())
                 .Build();
             
             users = usersList.ToList();
 
-            users.ForEach(x=> db.Users.Insert(x));
+            if(isBulkInsert)
+                db.Users.Insert(users);
+            else
+                users.ForEach(x=> db.Users.Insert(x));
+        }
+
+        private static void PrepareUsersWithTransaction(int userCreationCount, bool isBulkInsert, bool fail)
+        {
+            var usersList = Builder<User>.CreateListOfSize(userCreationCount)
+                .All()
+                    .With(x => x.Id = Guid.NewGuid())
+                .Build();
+
+            users = usersList.ToList();
+
+            if (fail)
+            {
+                var failUser = users.Last();
+                users.Add(failUser);
+            }
+
+            using (var tx = db.BeginTransaction())
+            {
+                try
+                {
+                    if(isBulkInsert)
+                        tx.Users.Insert(users);
+                    else
+                    {
+                        users.ForEach(x => tx.Users.Insert(x));        
+                    }
+                    
+                    tx.Commit();
+                }
+                catch(Exception e)
+                {
+                    System.Console.WriteLine("Exception: {0}", e.Message);
+                }
+            }
         }
         
         private static void FindOneUserWithoutPoco()
         {
-            var theUser = db.Users.FindById(users.First().Id);
-
-            System.Console.WriteLine("UserName: {0}, Email: {1}, FirstName: {2}, LastName: {3}",
-               theUser.UserName, theUser.EmailAddress, theUser.FirstName, theUser.LastName);
-
             System.Console.WriteLine();
             System.Console.WriteLine("************************************************************");
             System.Console.WriteLine();
+            
+            var theUser = db.Users.FindById(users.First().Id);
+
+            if (theUser == null)
+            {
+                System.Console.WriteLine("No Records Returned!");
+            }
+            else
+            {
+                System.Console.WriteLine("UserName: {0}, Email: {1}, FirstName: {2}, LastName: {3}",
+                theUser.UserName, theUser.EmailAddress, theUser.FirstName, theUser.LastName);
+            }
+
+            System.Console.WriteLine();
+            System.Console.WriteLine("************************************************************");
+            System.Console.WriteLine();    
         }
 
         private static void FindAllUsersWithPoco()
         {
             IList<User> theUsers = db.Users.All().ToList<User>();
-            
-            theUsers.ToList().ForEach(user=> System.Console.WriteLine("UserName: {0}, Email: {1}, FirstName: {2}, LastName: {3}",
+
+            if (theUsers == null)
+            {
+                System.Console.WriteLine("No Records Returned!");
+            }
+            else
+            {
+                theUsers.ToList().ForEach(user => System.Console.WriteLine("UserName: {0}, Email: {1}, FirstName: {2}, LastName: {3}",
                 user.UserName, user.EmailAddress, user.FirstName, user.LastName));
 
+            }
             System.Console.WriteLine();
             System.Console.WriteLine("************************************************************");
             System.Console.WriteLine();
@@ -77,12 +134,19 @@ namespace SimpleData.Console
         {
             var theUsers = db.Users.All();
 
-            foreach (var user in theUsers)
+            if (theUsers == null)
             {
-                System.Console.WriteLine("UserName: {0}, Email: {1}, FirstName: {2}, LastName: {3}",
-                user.UserName, user.EmailAddress, user.FirstName, user.LastName);
+                System.Console.WriteLine("No Records Returned!");
             }
+            else
+            {
 
+                foreach (var user in theUsers)
+                {
+                    System.Console.WriteLine("UserName: {0}, Email: {1}, FirstName: {2}, LastName: {3}",
+                        user.UserName, user.EmailAddress, user.FirstName, user.LastName);
+                }
+            }
             System.Console.WriteLine();
             System.Console.WriteLine("************************************************************");
             System.Console.WriteLine();
